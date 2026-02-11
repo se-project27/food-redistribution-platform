aa551dc8a281c66f195702ea97201ee0826a979e
Mithilesh Narne <mithileshnarne91@gmail.com>
2026-02-11 15:35:09 +0530

    Add edit profile component

---
+++ src/components/EditProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EditProfile = () => {
+  return (
+    <div>
+      <h3>Edit Profile</h3>
+      <input placeholder="Name" />
+      <button>Save</button>
+    </div>
+  );
+};
+
+export default EditProfile;
